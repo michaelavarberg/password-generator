0c2a3e34df17f8150361f9e1098e678def780e80
Michaela Varberg <michaelavarberg@gmail.com>
2022-08-04 18:11:57 -0600

    created initial createArray function

--- Develop/script.js
+++ Develop/script.js
@@ -14,10 +14,103 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var uppercase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowercase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialChars = [
+  " ",
+  '"',
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+  "]",
+];
+
 function generatePassword() {
-  //add prompt for length of password
+  //prompt for length of password
   var numChars = prompt("How many characters do you want your password to be?");
-  //add checks for at least 8 and no more than 128
+  //checks for at least 8 and no more than 128
   if (numChars < 8) {
     alert("Password must have at least 8 characters!");
     exit();
@@ -25,7 +118,7 @@ function generatePassword() {
     alert("Password must have less than 129 characters!");
     exit();
   }
-  //add prompt for do you want special characters, numbers, lowercase and uppercase
+  //prompt for do you want special characters, numbers, lowercase and uppercase
   wantsSpecial = confirm(
     "Click 'OK' if you want to include special characters in your password."
   );
@@ -40,7 +133,7 @@ function generatePassword() {
     "Click 'OK' if you want to include uppercase letters in your password."
   );
 
-  //add check for selecting at least one of the above
+  //check for selecting at least one of the above
   if (
     wantsSpecial === false &&
     wantsNumbers === false &&
@@ -52,57 +145,58 @@ function generatePassword() {
     );
     exit();
   }
-  //create arrays of each character type
+
   console.log(wantsSpecial);
   console.log(wantsNumbers);
   console.log(wantsLowercase);
   console.log(wantsUppercase);
   console.log(numChars);
   // create password from these arrays
-  var uppercase = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  var numChars = 8;
-  //working but arrays need to be defined before
+  array = [];
+  //create array including all possible character values based on user preferences
+  function createArray(special, nums, lower, upper) {
+    if (special) {
+      var array = specialChars;
+      return array;
+    }
+    console.log(array);
+    if (nums) {
+      array = array.concat(numbers);
+      return array;
+    }
+    console.log(array);
+    if (lower) {
+      array = array.concat(lowercase);
+      return array;
+    }
+    console.log(array);
+    if (upper) {
+      array = array.concat(uppercase);
+      return array;
+    }
+  }
+  console.log(array);
+
+  //create actual password when given final array
   function randomIndex(anyArray) {
     var result = " ";
     for (var i = 0; i < numChars; i++) {
       var randIndex = Math.floor(Math.random() * anyArray.length);
       result = result + anyArray[randIndex];
-      console.log(randIndex);
     }
     return result;
   }
 
+  var finalArray = createArray(
+    wantsSpecial,
+    wantsNumbers,
+    wantsLowercase,
+    wantsUppercase
+  );
   //make sure checks pick up where left off instead of quitting
   password = randomIndex(finalArray);
-  console.log(password);
-  console.log(numChars);
+  // console.log(password);
+  // console.log(numChars);
   return password;
 }
 
